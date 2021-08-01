f0a09b20be8e14a952792f34e84207cf4cb0cd2c
Rahat Hossain <rahathossain690@gmail.com>
2021-08-01 08:48:53 +0600

    active list cleared

--- src/popup.js
+++ src/popup.js
@@ -105,14 +105,6 @@ document.getElementById('div_3').addEventListener('change', () => {
     })
 })
 
-document.getElementById("khuljasimsim").addEventListener('dblclick', () => {
-    if(document.getElementById("hidden_cell").style.display == "none") {
-        document.getElementById("hidden_cell").style.display = "block"
-    } else {
-        document.getElementById("hidden_cell").style.display = "none"
-    }
-})
-
 function contest_data() { //console.log('called')
     chrome.storage.sync.get(['DATA'], par_data => {
         const data = par_data.DATA
@@ -126,7 +118,6 @@ function contest_data() { //console.log('called')
                 if(total_contests !== 0) elem.innerHTML += '<hr style="background-color: #cfcfcf">'
                 document.getElementById('contest_data').appendChild(elem)
             })
-            document.getElementById("active_devices").innerHTML = `Active devices: ${data.active_devices}`
         } else{
             document.getElementById('contest_data').innerHTML = ''
             let elem = document.createElement('h3')
